4a135201e9b9b0cee1d2b9b9560c5dad39f8b5be
tm <tm@mse.bht.de>
2022-11-12 13:22:30 +0100

    Changed compiler config

--- src/main/java/de/bht/mvnex/Application.java
+++ src/main/java/de/bht/mvnex/Application.java
@@ -1,6 +1,20 @@
 package de.bht.mvnex;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
+    
+    public void greet(){
+        List<String> greetings = new ArrayList<>();
+        greetings.add("Moderne Softwareentwicklung");
+
+        for (String greeting : greetings) {
+            System.out.println(greeting);
+        }
+    }
+
+
     
     public Application() {
         System.out.println ("Inside Application");
@@ -10,5 +24,6 @@ public class Application {
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	    Application app = new Application();
+        app.greet();
     }
 }
